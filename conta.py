e5079b85f76f4f521949246ee7d8e1d6190bf850
carolinamaciel <anna.maciel@conceptu.ind.br>
2020-02-25 16:50:59 -0300

    Criando comportamento para a classe Conta

--- conta.py
+++ conta.py
@@ -6,3 +6,14 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print("{} seu saldo é {}".format(self.titular, self.saldo))
+
+    def deposita(self, valor):
+        self.saldo += valor
+        print("Você está depositando {} na conta de {}".format(valor, self.titular))
+
+    def saca(self, valor):
+        self.saldo -= valor
+        print("{} você está retindo {} de sua conta.".format(self.titular, valor))
