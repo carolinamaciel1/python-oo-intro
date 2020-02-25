eaf36b7530f0152e80118e04abb65a5a122bd356
carolinamaciel <anna.maciel@conceptu.ind.br>
2020-02-25 12:53:10 -0300

    paradigma procedural

---
+++ teste.py
@@ -0,0 +1,15 @@
+def criar_conta(numero, titular, saldo, limite):
+    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
+    return conta
+
+
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+
+def sacar(conta, valor):
+    conta["saldo"] -= valor
+
+
+def extrato(conta):
+    print("O saldo é {}".format(conta["saldo"]))
