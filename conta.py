800144fc707fb358aa147287c3d15619d962bee8
carolinamaciel <anna.maciel@conceptu.ind.br>
2020-02-25 21:04:01 -0300

    Encapsulamento de atributos e criação do novo método transfere

--- conta.py
+++ conta.py
@@ -2,18 +2,23 @@ class Conta:
 
     def __init__(self, conta, titular, saldo, limite):
         print("{} seu número de conta é {} seu saldo é {} de um limite de {} ".format(titular, conta, saldo, limite))
-        self.conta = conta
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        # O caractere _ serve para encapsular os atributos de uma classe
+        self.__conta = conta
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("{} seu saldo é {}".format(self.titular, self.saldo))
+        print("{} seu saldo é {}".format(self.__titular, self.__saldo))
 
     def deposita(self, valor):
-        self.saldo += valor
-        print("Você está depositando {} na conta de {}".format(valor, self.titular))
+        self.__saldo += valor
+        print("Você está depositando {} na conta de {}".format(valor, self.__titular))
 
     def saca(self, valor):
-        self.saldo -= valor
-        print("{} você está retindo {} de sua conta.".format(self.titular, valor))
+        self.__saldo -= valor
+        print("{} você está retindo {} de sua conta.".format(self.__titular, valor))
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
